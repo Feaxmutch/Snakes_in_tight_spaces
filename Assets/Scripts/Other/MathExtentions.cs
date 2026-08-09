d0d54f34cda2133c66df22881cfac5b9eb52264a
Feaxmutch <118442920+Feaxmutch@users.noreply.github.com>
2026-08-09 21:48:01 +0300

    Update MathExtentions

--- Assets/Scripts/Other/MathExtentions.cs
+++ Assets/Scripts/Other/MathExtentions.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace Other
 {
     public static class MathExtentions
     {
-        public static bool IsNegative(this int value) => value < 0;
+        public static bool IsNegative<T>(this T value) where T : struct, IComparable
+        {
+            return value.CompareTo(default) < 0;
+        }
+
+        public static bool IsInRange<T>(this T value, T min, T max) where T : struct, IComparable
+        {
+            return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+        } 
     }
 }
